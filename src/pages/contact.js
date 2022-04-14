7e6a0aff3cfea8b9fdbcb0af01f9145bdfcb9782
William Chan <wc2184@nyu.edu>
2022-04-14 15:52:05 -0400

    updated cuz professional

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,7 +12,7 @@ function ContactPage(props) {
         bgImage=""
         bgImageOpacity={1}
         title="Contact Us"
-        subtitle="Feel free to ask ANY question. We will answer!"
+        subtitle="Feel free to ask any question. We answer all questions."
         buttonText="Send message"
         buttonColor="primary"
         showNameField={true}
