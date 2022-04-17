ac3fb1617282893414e0edc27f50ad4de2ae8f38
wc2184 <68214261+wc2184@users.noreply.github.com>
2022-04-17 09:42:34 -0400

    edit

--- src/components/BehindLogin/ContractGenerate.js
+++ src/components/BehindLogin/ContractGenerate.js
@@ -35,8 +35,8 @@ function Contact(props) {
     // Show pending indicator
     setPending(true);
     console.log(data);
-    data.minutes = String(minutes);
-    data.hours = String(hours);
+    data.minutes = minutes.toString();
+    data.hours = hours.toString();
     data.penalty = penalty;
     data.type = "contract";
     console.log(data);
