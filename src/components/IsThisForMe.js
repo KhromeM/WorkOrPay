e5a6e43a145626b0aa42dab4fe245f9c128f4f31
KhromeM <68210337+KhromeM@users.noreply.github.com>
2022-04-19 15:45:16 -0400

    m

---
+++ src/components/IsThisForMe.js
@@ -0,0 +1,98 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import { emphasize } from "@material-ui/core/styles/colorManipulator";
+import Section from "./Section";
+import SectionHeader from "./SectionHeader";
+
+
+const useStyles = makeStyles((theme) => ({
+  gridItem: {
+    // Add border that contrasts lightly with background color.
+    // We use boxShadow so that it's hidden around outer edge
+    // due to container <Card> having overflow: hidden
+    boxShadow: `1px 1px 0 0 ${emphasize(theme.palette.background.paper, 0.08)}`,
+    textAlign: "center",
+  },
+}));
+
+function IsThisForMe(props) {
+  const classes = useStyles();
+
+  return (
+    <Section
+      bgColor={props.bgColor}
+      size={props.size}
+      bgImage={props.bgImage}
+      bgImageOpacity={props.bgImageOpacity}
+    >
+      <Container>
+        <SectionHeader
+          title='How It Works'
+          subtitle=''
+          size={4}
+          textAlign="center"
+        />
+        <Container
+          maxWidth="md"
+          disableGutters={true}
+          className={classes.itemsContainer}
+        >
+            <Grid
+              className={classes.row}
+              container={true}
+              item={true}
+              alignItems="center"
+              spacing={4}
+            >
+              <Grid item={true} xs={12} md={6}>
+                <Box
+                  textAlign={{
+                    xs: "center",
+                    md: "left",
+                  }}
+                >
+                  <Typography variant="h4" gutterBottom={true}>
+                  1) Sign Up
+                  </Typography>
+                  <Typography className={classes.description} variant="h6">
+                  First sign up for an account and purchase the monthly membership. Once you have a membership you will be able to form contracts.
+                  </Typography>
+
+                  <Typography variant="h4" gutterBottom={true}>
+                  2) Form A Contract
+                  </Typography>
+                  <Typography className={classes.description} variant="h6">
+                  Concretely define your goal. Make it  measurable. Set a hard deadline. Give yourself enough time to complete your objectives, but not enough time for you to slack off. Example: "I will finish creating and launch my website in 8 days!
+                  </Typography>
+
+                  <Typography variant="h4" gutterBottom={true}>
+                  3) Set Financial Penalties 
+                  </Typography>
+                  <Typography className={classes.description} variant="h6">
+                  If just making goals was enough, you wouldn't be here. Create extra motivation for yourself by wagering your money. Choose an amount from $25-$1000. Choose the charity or anti-charity that gets your money if you fail and deposit the amount.
+                  </Typography>
+
+                  <Typography variant="h4" gutterBottom={true}>
+                    Follow Through
+                  </Typography>
+                  <Typography className={classes.description} variant="h6">
+                  Now you have until your deadline to reach your goal. Once you do, send us proof so we can verify it and send your deposit back. If you fail to complete your goal by the deadline, your deposit is donated.
+                  </Typography>
+
+                  
+                </Box>
+              </Grid>
+            </Grid>
+
+        </Container>
+      </Container>
+    </Section>
+  );
+}
+
+export default IsThisForMe;
