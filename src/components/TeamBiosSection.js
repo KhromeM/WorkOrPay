1812260362cbb6c33c646f244ef89d6e706f949c
William Chan <68214261+wc2184@users.noreply.github.com>
2023-05-06 00:14:38 -0400

    Update TeamBiosSection.js

--- src/components/TeamBiosSection.js
+++ src/components/TeamBiosSection.js
@@ -37,12 +37,12 @@ function TeamBiosSection(props) {
       link: "LinkedIn",
       bio: "For my whole life I have been dealing with burst of motivation where I am extremely productive for several days, followed by weeks long slumps. This yo-yo behavior does not let a person develop their skills or build anything substantial. According to my observations, people usually start working towards a goal following a temporary spike in motivation. It lasts a few days at most. Usually, it is not enough for people to move past the hurdle that is the first 2 weeks. In the first 2 weeks of starting anything new, you are garbage at everything. It hard work and not very rewarding because you are constantly getting things wrong and not being able to get in flow. However, after 2 weeks so, People start appreciating the task. They finally see hints of progression of their abilities. Are able to work for longer stretches at a time before running into something frustrating. This allows them to get into flow. Together this starts to produce consistent internal motivation that allows the person to actually stick with the new habit long term. But 99% of the time people give up before getting here. To address this issue, I created WorkOrPay",
     },
-    // {
-    //   avatar: "",
-    //   name: "William Chan",
-    //   role: "Add your link",
-    //   bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum consequatur numquam aliquam tenetur ad amet inventore hic beatae, quas accusantium perferendis sapiente explicabo, corporis totam! Labore reprehenderit beatae magnam animi!",
-    // },
+    {
+      avatar: "",
+      name: "William Chan",
+      role: "LinkedIn",
+      bio: "As someone who has always found it challenging to stick to long-term commitments that don't provide instant dopamine hits, I realized that the key to success lies in developing consistent internal motivation. However, I also recognize that getting to that point can be a huge obstacle, particularly during those first few weeks when the task seems frustrating and unproductive. To combat this, I created WorkOrPay, a tool that helps individuals stay accountable for their goals and habits by allowing them to set monetary consequences for not following through. By putting a financial cost on failure, WorkOrPay incentivizes individuals to push through those difficult initial stages and start experiencing the benefits of continued effort. Through consistent practice, habits can be formed, skills can be developed, and lasting change can be achieved. WorkOrPay is the solution for anyone who struggles with sticking to long-term commitments or finds it difficult to motivate themselves without immediate gratification.",
+    },
   ];
 
   return (
@@ -81,7 +81,7 @@ function TeamBiosSection(props) {
                         component="p"
 
                       >
-                        <a target='_blank' href="https://www.linkedin.com/in/khurram-mustafa-435b16177/">{item.link}</a>
+                        <a target='_blank' href={item.name == "Khurram Mustafa" ? "https://www.linkedin.com/in/khurram-mustafa-435b16177/" : "https://williamchan.surge.sh/linkedin"}>{item.link}</a>
                       </Typography>
                     <Box mt={2}>
                       <Typography variant="body1" component="p">
